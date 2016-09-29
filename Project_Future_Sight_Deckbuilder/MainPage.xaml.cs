81b0c45e6e7f8fa25651f92956b50d49c19ea988
croch <croch@DESKTOP-8EV6BAS>
2016-09-29 00:22:10 -0400

    Created the pages for each view, and added functionality to the menu

--- Project_Future_Sight_Deckbuilder/MainPage.xaml.cs
+++ Project_Future_Sight_Deckbuilder/MainPage.xaml.cs
@@ -25,16 +25,50 @@ namespace Project_Future_Sight_Deckbuilder
         public MainPage()
         {
             this.InitializeComponent();
+
+            Home.IsSelected = true;
+            BackButton.Visibility = Visibility.Collapsed;
         }
 
         private void HamburgerButton_Click(object sender, RoutedEventArgs e)
         {
+            Splitview.IsPaneOpen = !Splitview.IsPaneOpen;
+        }
 
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MenuFrame.CanGoBack)
+            {
+                MenuFrame.GoBack();
+                Home.IsSelected = true;
+            }
         }
 
         private void MenuList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            if (Home.IsSelected)
+            {
+                BackButton.Visibility = Visibility.Collapsed;
+                MenuFrame.Navigate(typeof(Home));
+            }
+            else if (Add.IsSelected)
+            {
+                BackButton.Visibility = Visibility.Visible;
+                MenuFrame.Navigate(typeof(AddDeck));
+            }
+            else if (EditDecks.IsSelected)
+            {
+                BackButton.Visibility = Visibility.Visible;
+                MenuFrame.Navigate(typeof(Editor));
+            }
+            else if (Inventory.IsSelected)
+            {
+                BackButton.Visibility = Visibility.Visible;
+                MenuFrame.Navigate(typeof(Inventory));
+            }
+            
         }
+
+       
     }
 }
